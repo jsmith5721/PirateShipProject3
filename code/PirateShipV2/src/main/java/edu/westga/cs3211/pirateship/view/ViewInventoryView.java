a9a14a452d4d6a031f8956f94cb5ed453900642a
Kirya Duncan II <kdunca10@my.westga.edu>
2025-12-08 10:19:21 -0500

    Added functionality to "back" button to return to Landing Page from View Inventory Page.

--- code/PirateShipV2/src/main/java/edu/westga/cs3211/pirateship/view/ViewInventoryView.java
+++ code/PirateShipV2/src/main/java/edu/westga/cs3211/pirateship/view/ViewInventoryView.java
@@ -3,13 +3,18 @@ package edu.westga.cs3211.pirateship.view;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
+import javafx.stage.Stage;
 import edu.westga.cs3211.pirateship.model.Stock;
 import edu.westga.cs3211.pirateship.model.Ship;
-import edu.westga.cs3211.pirateship.viewmodel.AddContainerVM;
 import edu.westga.cs3211.pirateship.viewmodel.ViewInventoryVM;
 
 import java.time.LocalDate;
@@ -35,36 +40,57 @@ public class ViewInventoryView {
 	private TableColumn<Stock, LocalDate> expCol;
 
 	private ViewInventoryVM viewModel;
+	
+	private Ship ship;
 
-
+	/*
+	 * Sets the given ship as the current ship for the inventory
+	 */
 	public void setShip(Ship ship) {
+		this.ship = ship;
 		this.viewModel = new ViewInventoryVM(ship);
 		this.bindViewModel();
 	}
 
+	/*
+	 * Sets the scene to show the landing page once more
+	 */
+	public void returnToLandingPage(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(LandingPage.class.getResource("LandingPage.fxml"));
+			loader.load();
+			LandingPage landingPageController = loader.getController();
+			landingPageController.startup(this.ship.getCurrentUser());
+			
+			Parent root = loader.getRoot();
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.setTitle("Flying Dutchman - Home");
+	        stage.show();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Error loading Landing Page.");
+            alert.showAndWait();
+        }
+	}
+	
 	private void bindViewModel() {
-		nameCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
+		this.nameCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
 
-		qtyCol.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getQuantity()));
+		this.qtyCol.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getQuantity()));
 
-		conditionCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getCondition().toString()));
+		this.conditionCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getCondition().toString()));
 
-		specialCol.setCellValueFactory(
+		this.specialCol.setCellValueFactory(
 				cell -> new SimpleStringProperty(cell.getValue().getSpecialQualities().toString()));
 
-		expCol.setCellValueFactory(cell -> new SimpleObjectProperty<>(cell.getValue().getExpirationDate()));
+		this.expCol.setCellValueFactory(cell -> new SimpleObjectProperty<>(cell.getValue().getExpirationDate()));
 
-		Ship ship = getCurrentShipSomehow(); // see note below
-		this.viewModel.loadEntireInventory(ship);
+		this.viewModel.loadEntireInventory(this.ship);
 
-		stockTable.setItems(this.viewModel.getStocks());
-	}
-
-	/**
-	 * TEMP placeholder – replace this with however your app gets the current Ship.
-	 * Could be a singleton, passed from LandingPage, from Authenticator, etc.
-	 */
-	private Ship getCurrentShipSomehow() {
-		return null;
+		this.stockTable.setItems(this.viewModel.getStocks());
 	}
 }
--- code/PirateShipV2/src/main/java/module-info.java
+++ code/PirateShipV2/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.westga.cs3211.pirateship {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+	requires javafx.base;
 
     opens edu.westga.cs3211.pirateship.view to javafx.fxml;
     exports edu.westga.cs3211.pirateship;
